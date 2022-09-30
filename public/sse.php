0c6e7cc4d4ceb3b4707fbe64699bb8cbd8ae9710
blue32a <blue32a.jp@gmail.com>
2022-09-30 23:35:43 +0900

    サーバーサイドの状況をログに出力

--- public/sse.php
+++ public/sse.php
@@ -1,10 +1,15 @@
 <?php
+function debug($message) {
+    file_put_contents('../logs/app.log', $message . "\n", FILE_APPEND);
+}
 
+ignore_user_abort(false);
 header('Cache-Control: no-store');
 header('Content-Type: text/event-stream');
 
 while (true) {
     $curDate = date(DATE_ISO8601);
+    debug($curDate);
     echo 'data: This is a message at time ' . $curDate, "\n\n";
 
     while (ob_get_level() > 0) {
@@ -14,8 +19,11 @@ while (true) {
     flush();
 
     if (connection_aborted()) {
+        debug('connection aborted');
         break;
     }
 
     sleep(1);
 }
+
+debug('exit');
