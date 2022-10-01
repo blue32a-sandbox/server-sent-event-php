e747b86303609c38b7fd0e7806483ab33407afa7
blue32a <blue32a.jp@gmail.com>
2022-10-01 13:49:29 +0900

    新しいメッセージを送信

--- public/sse.php
+++ public/sse.php
@@ -3,14 +3,27 @@ function debug($message) {
     file_put_contents('../logs/app.log', $message . "\n", FILE_APPEND);
 }
 
-ignore_user_abort(false);
+ignore_user_abort(true);
 header('Cache-Control: no-store');
 header('Content-Type: text/event-stream');
+
+$id = 0;
 
 while (true) {
     $curDate = date(DATE_ISO8601);
     debug($curDate);
-    echo 'data: This is a message at time ' . $curDate, "\n\n";
+
+    // event: messageとして送信
+    echo 'id: ' . ++$id . "\n";
+    echo 'data: This is a message at time ' . $curDate . "\n";
+    echo 'data: hoge' . "\n";
+    echo "\n"; // 2つの改行文字で区切られる
+
+    // 新しいメッセージ
+    echo 'id: ' . ++$id . "\n";
+    echo "event: ping\n";
+    echo 'data: {"time": "' . $curDate . '", "text": "This is ping."}';
+    echo "\n\n";
 
     while (ob_get_level() > 0) {
         ob_end_flush();
@@ -18,6 +31,7 @@ while (true) {
 
     flush();
 
+    // 何故か機能しない・・・
     if (connection_aborted()) {
         debug('connection aborted');
         break;
